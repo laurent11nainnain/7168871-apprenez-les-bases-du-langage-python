55cce26c4e7b5adac25f41646d029dd014a804fa
laurent11nainnain <167130864+laurent11nainnain@users.noreply.github.com>
2024-04-16 11:25:40 +0200

    Update operations.py

--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,19 @@
-# Écrivez votre code ici !
+def sum(a, b, c):
+    return a + b + c
+
+def subtraction(a, b, c):
+    return a - b - c
+
+def multiplication(a, b, c):
+    return a * b * : c
+
+def division(a, b, c):
+    if a != 0:
+        return a / b / c
+    if b != 0:
+         return a / b / c
+    if c != 0:
+        return a / b / c
+    else:
+        print("erreur : division par zéro")
+        
